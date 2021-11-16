854ed0928bfdfe64e4daf7e09f7e54af1e0e59b5
Ayoub <ayoubhaddou1@gmail.com>
2021-11-16 17:02:53 +0100

    Ce quon a fait en cours

--- fonction.py
+++ fonction.py
@@ -3,7 +3,9 @@ def attack(x) :
     ''' Function to attack enemy by random between 5 and 10   
     ---------------  
     x should be an int  (Like x = enemy_HP) '''
-    return x - random.randint(5,10) 
+    x -= random.randint(5,10) 
+    return x 
+
 
 
 def attack_lightning(x):              # x =  Les points de vie ennemie 
@@ -49,7 +51,21 @@ def pop_enemy():
     enemy_list = []
     name_list= ["Pythosore","Devosore","Simplosore","Tiranosor","Bigbob","Jevaismourir"]
     while c < random.randint(1,3):
-        enemy_list.append({ "Name"+str(c+1) : random.choice(name_list) , "PV" : random.randint(35,50)})
+        enemy_list.append({ "Name" : random.choice(name_list) , "HP" : random.randint(35,50)})
         c += 1 
     return enemy_list
+
+
  
+ # Déclaration de nos variables 
+degats = 8
+my_hp = 50
+round = 1
+enemy_hp = 30
+ennemy_degats = 5
+# prenom = input("Quel est votre prenom ?")
+# choix_principal = input("Menu principal : \n Quel est votre choix ? \n 1 - start  \n 2 - score \n 3 - quitter ")
+# choix_combat = input (" Que voulez-vous faire ? \n option 1 : Attaque \n option 2 : Inventaire \n option 3 : fuir (Quitter la partie")
+# choix_attaque = input (f"Que voulez_vous faire {prenom} ? \n option 1 : Attaque corp à corp \n option 2 : Attaque éclair \n option 3 : Attaque groupée ") 
+# choix_inventaire = input ("Que voulez-vous faire ? \n option 1 : utiliser potion \n option 2 : Utiliser maxipotion")
+# choix_enemy = input ("Qui voulez-vous attaquer : ")
--- main.py
+++ main.py
@@ -1,19 +1,142 @@
 # import 
 import fonction 
 import random 
-
-# Déclaration de nos variables 
-degats = 8
-my_hp = 50
-round = 1
-enemy_hp = 30
-ennemy_degats = 5
-prenom = input("Quel est votre prenom ?")
-choix_principal = input("Menu principal : \n Quel est votre choix ? \n 1 - start  \n 2 - score \n 3 - quitter ")
-choix_combat = input (" Que voulez-vous faire ? \n option 1 : Attaque \n option 2 : Inventaire \n option 3 : fuir (Quitter la partie")
-choix_attaque = input (f"Que voulez_vous faire {prenom} ? \n option 1 : Attaque corp à corp \n option 2 : Attaque éclair \n option 3 : Attaque groupée ") 
-choix_inventaire = input ("Que voulez-vous faire ? \n option 1 : utiliser potion {} \n option 2 : Utiliser maxipotion {}")
+import sys 
 
 # commencement de la boucle : 
 # leave = True 
-# while leave : 
+enemies = fonction.pop_enemy()
+leave = True 
+while leave :
+
+    prenom = input("Quel est votre prenom ?")
+    print(f"Bonjour {prenom}")
+
+    choix_principal = input("Menu principal : \n Quel est votre choix ? \n 1 - start  \n 2 - score \n 3 - quitter \n ")
+
+    if choix_principal == '1' :
+        print(f"le royaume se fait attaqué defendez le royaume de VS Code, tout les dev dependent de vous {prenom}")
+        vague = 1 
+        player = {"Name" : prenom , "potion" : 3, "potion_max" : 0, "HP" : 55}
+        
+
+        round = 0
+        while player["HP"] >= 0 :
+            print("Round :" , round)
+            print(f"{prenom} : ", player["HP"] ," HP // Potion : ", player["potion" ] ," // Potion max : ", player["potion_max"])
+            cnt = 1 
+            print("Monstre(s) sur le terrain :")
+            for monstre in enemies:
+                print(monstre["Name"] , " " , monstre["HP"] , " HP")
+            choix_combat = input (" Que voulez-vous faire ? \n option 1 : Attaque \n option 2 : Inventaire \n option 3 : fuir (Quitter la partie) \n ")
+            # if choix_combat == "3" :
+                # sys.exit("Vous avez quitter")
+            combat = True 
+            while combat :
+
+                if choix_combat == "1" :
+
+                    choix_attaque = input (f"Que voulez_vous faire {prenom} ? \n option 1 : Attaque corp à corp \n option 2 : Attaque éclair \n option 3 : Attaque groupée \n ") 
+                    if choix_attaque == "1" :
+                        cnt = 1 
+                        for monstre in enemies:
+                            print(str(cnt) + " " + monstre["Name"] + " " + str(monstre["HP"]))
+                            cnt += 1 
+                        choix_enemy = input("Qui voulez-vous attaquer : ")
+                        enemies[int(choix_enemy) - 1]["HP"] = fonction.attack(enemies[int(choix_enemy) - 1]["HP"])
+                        diff = player["HP"] 
+                        player["HP"] = fonction.enemy_attack(player["HP"])
+                        print(f"L'ennemi vous a attaqué \n{prenom} a perdu : ", diff - player["HP"], "HP" ,"\n") 
+                        round += 1            
+                        break
+
+                    elif choix_attaque == "2" :
+                        choix_enemy = input("Qui voulez-vous attaquer : ")
+                        enemies[int(choix_enemy) - 1]["HP"] = fonction.attack_lightning(enemies[int(choix_enemy) - 1]["HP"])
+                        diff = player["HP"] 
+                        player["HP"] = fonction.enemy_attack(player["HP"])
+                        print(f"L'ennemi vous a attaqué \n{prenom} a perdu : ", diff - player["HP"], "HP" ,"\n")   
+                        round += 1          
+                        break
+
+                    elif choix_attaque == "3" :
+                        for i in range(len(enemies)):
+                            enemies[i]["HP"] = fonction.attack_group(enemies[i]["HP"])
+                        diff = player["HP"] 
+                        player["HP"] = fonction.enemy_attack(player["HP"])
+                        print(f"L'ennemi vous a attaqué \n{prenom} a perdu : ", diff - player["HP"], "HP" ,"\n")     
+                        round += 1        
+                        break 
+                    break
+
+                    
+                elif choix_combat == "2" :
+                    print(f" Dans votre inventaire il y a " + str(player["potion"]) + " potion(s)")
+                    choix_inventaire = input ("Que voulez-vous faire ? \n option 1 : utiliser potion \n option 2 : Utiliser maxipotion \n option 3 : Retour en arriere ")
+
+                
+                    if choix_inventaire == "1" : 
+                        if player["potion"] > 0 : 
+                            player["HP"] = fonction.potion(player["HP"])
+                            player["potion"] -= 1
+                            print ( " Vous avez : ", player["HP"] ," point de vie", "et " , player["potion"] , "potion(s)")
+                            diff = player["HP"] 
+                            player["HP"] = fonction.enemy_attack(player["HP"])
+                            print(f"L'ennemi vous a attaqué \n{prenom} a perdu : ", diff - player["HP"], "HP" ,"\n")     
+                            round += 1        
+                            break
+                        else: 
+                            print("Vous n'avez plus de potion!")
+
+
+                    elif choix_inventaire == "2" :
+                        if player["potion_max"] > 0 :
+                            player["HP"] = fonction.max_potion(player["HP"]) 
+                            player["potion_max"] -= 1
+                            print ( " Vous avez : ", player["HP"] ," point de vie", "et " , player["potion_max"] , "potion(s)") 
+                            diff = player["HP"] 
+                            player["HP"] = fonction.enemy_attack(player["HP"])
+                            print(f"L'ennemi vous a attaqué \n{prenom} a perdu : ", diff - player["HP"], "HP" ,"\n")   
+                            round += 1          
+                            break 
+                        else:
+                            print("Mince! Vous n'avez plus de potion!")                       
+
+
+                    elif choix_inventaire == "3" :
+                        break 
+                                                
+                            
+
+                    elif choix_combat == "3" : 
+                        sys.exit("Vous avez quitter") 
+
+
+    elif choix_principal == "3" : 
+        sys.exit("Vous avez quitter")  
+
+                
+
+
+                    
+                        
+                                                    
+
+                        
+
+
+
+
+
+
+
+
+
+
+    elif choix_principal == 2:
+        print("Score =")
+
+    elif choix_principal == 3 :
+        leave = False
+        
+        
